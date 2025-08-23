9ebe7aeacbe2873948c0b0e1dbea258baee0cac3
Muqaddas Mehboob <muqaddasmehboob7@gmail.com>
2025-08-23 01:48:52 -0700

    Add Navbar and Hero Section

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
 export default [index("routes/home.tsx")] satisfies RouteConfig;
+
+
+// This is the file of React Router Configuration. This file contains all the routing to be
+// loaded in your website. If you have to add more routes, you have to expand this file.
