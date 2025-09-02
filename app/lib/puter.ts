18f880eee7eb6a4d04fef3f8375791b59f5b5728
Muqaddas Mehboob <muqaddasmehboob7@gmail.com>
2025-09-02 10:47:39 -0700

    Update files

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -132,10 +132,10 @@ interface PuterStore {
 const getPuter = () : typeof window.puter | null => 
     typeof window !== undefined && window.puter ? window.puter : null;
 
-export const usePuterStore = create<PuterStore>((set, get)) => {
-    const setError = () => {
-        set({
-            
-        })
-    }
-}
+// export const usePuterStore = create<PuterStore>((set, get)) => {
+//     const setError = () => {
+//         set({
+
+//         })
+//     }
+// }
